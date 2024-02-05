53185ef555d286d8f5bda37e3c3e34b47b63e88e
Daniil Tsioma <dev.dtsioma@gmail.com>
2024-02-05 14:51:08 -0800

    connect to OpenAI

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+# libraries
 import pyaudio
 from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+# load environmental variables
+load_dotenv()
+
+# get OpenAI API key
+openai_api_key = os.getenv('OPENAI_API_KEY')
+
+# set up OpenAI client
+client = OpenAI(api_key=openai_api_key)
